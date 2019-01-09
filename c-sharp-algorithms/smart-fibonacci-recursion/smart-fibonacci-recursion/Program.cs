45cc200530cca9173bc0652b570b07c7bdc920e5
MikaelAbehsera <Mikael.abehsera@gmail.com>
2019-01-08 22:06:38 -0500

    smart fib counter

--- c-sharp-algorithms/smart-fibonacci-recursion/smart-fibonacci-recursion/Program.cs
+++ c-sharp-algorithms/smart-fibonacci-recursion/smart-fibonacci-recursion/Program.cs
@@ -8,23 +8,31 @@ namespace smart_fibonacci_recursion
 	{
 	class Program
 		{
+		//Question: How many additions are necessary to calculate the  with smart_fibonacci(1000)?
 
 		static int SmartFibonacci (int num) {
 			int a = 0;
 			int b = 1;
 			
+			if(num == 0) {
+				return a + 0;
+			}
+
+			int additions = 0;
 
 			for (int i = 0; i < num; i++) {
 				a = b;
 				b = a + b;
+				additions++;
 			}
-			return b;
+
+			return b + additions - 1;
 		}
 
 		static void Main ( string[] args )
 			{
 
-			Console.WriteLine ( SmartFibonacci(1000) );
+			Console.WriteLine ( SmartFibonacci(40) );
 			Console.ReadKey ( );
 			}
 		}
