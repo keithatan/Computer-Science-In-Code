6942bf274d97b52cd9f9740b75532ffba5ec53b2
MikaelAbehsera <Mikael.abehsera@gmail.com>
2019-01-08 21:32:01 -0500

    template for fib operation counter

---
+++ c-sharp-algorithms/fibonacci-operation-counter/fibonacci-operation-counter/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacci_operation_counter
+	{
+	class Program
+		{
+
+		/// <summary>
+		/// This function will count how many operations are ran when using the fibonacci solving recusive method
+		/// </summary>
+		/// <param name="num"></param>
+		/// <returns></returns>
+		static int FibonacciOperationsCounter(int num){
+
+		}
+
+		static void Main ( string[] args )
+			{
+			
+			Console.WriteLine (FibonacciOperationsCounter(10));
+			Console.ReadKey ( ); 
+			}
+		}
+	}
---
+++ c-sharp-algorithms/fibonacci-operation-counter/fibonacci-operation-counter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle ( "fibonacci-operation-counter" )]
+[assembly: AssemblyDescription ( "" )]
+[assembly: AssemblyConfiguration ( "" )]
+[assembly: AssemblyCompany ( "" )]
+[assembly: AssemblyProduct ( "fibonacci-operation-counter" )]
+[assembly: AssemblyCopyright ( "Copyright ©  2019" )]
+[assembly: AssemblyTrademark ( "" )]
+[assembly: AssemblyCulture ( "" )]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible ( false )]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid ( "d5f0eb53-918e-4c12-a59c-0e1ec66d87bb" )]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion ( "1.0.0.0" )]
+[assembly: AssemblyFileVersion ( "1.0.0.0" )]
