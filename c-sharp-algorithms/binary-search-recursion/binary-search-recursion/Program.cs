7e2a69d56a136979dd704f1c91189868596afd87
MikaelAbehsera <Mikael.abehsera@gmail.com>
2019-01-09 22:52:26 -0500

    converted int to double, floored, then back to int

--- c-sharp-algorithms/binary-search-recursion/binary-search-recursion/Program.cs
+++ c-sharp-algorithms/binary-search-recursion/binary-search-recursion/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace binary_search_recursion
 	{
 	class Program
@@ -14,21 +13,66 @@ namespace binary_search_recursion
 		/// <param name="source">An array of integers.</param>
 		/// <param name="search">The number to find in the array.</param>
 		/// <returns>True if found, False if not found.</returns>
-		static int binarySearch(int[] source, int search) {
+		static bool BinarySearchAlgorithm ( int[] source, int search ) {
 			//split the array by the middle number
-			int middle = Math.Floor ( source.Length / 2 );
+			int middle = Convert.ToInt16(Math.Floor(Convert.ToDouble(Convert.ToDouble( source.Length / 2 ))));
 
-			if (source[middle] == search) {
+			if( source[middle] == search )
+				{
 				// 
 				return true;
-			}
+
+				}
+			else if( source.Length <= 1 )
+				{
+
+				return false;
+
+				}
+			else
+				{
+
+				if( source[middle] < search )
+					{
+					// if middle is smaller then we go right
+					int[] right = source.Skip(middle).ToArray();
+
+					BinarySearchAlgorithm(right, search);
+
+					}
+				else if( search < source[middle] )
+					{
+					// if middle is smaller then we go left 
+					int[] left = source.Take(middle).ToArray();
+
+					BinarySearchAlgorithm(left, search);
+
+					}
+				else
+					{
+					return false;
+					}
+				}
 		}
 
+		/// <summary>
+		///  Function will generate a list for testing
+		/// </summary>
+		/// <param name="cout"></param>
+		/// <returns></returns>
+		static int[] listMaker(int cout) {
+				List<int> list = new List<int>();
+				for( int i = 0; i < cout; i++ )
+					list.Add(i);
+				return list.ToArray();
+			}
+
 		static void Main ( string[] args )
-			{
+			{  
+			 
 			// The code provided will print ‘Hello World’ to the console.
 			// Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-			Console.WriteLine ( "Hello World!" );
+			Console.WriteLine (BinarySearchAlgorithm(listMaker(100), 23 ));
 			Console.ReadKey ( );
 
 			// Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
