095abbb360dd9309f7c861698c593e8dd7d5089c
Mikael Abehsera <mikael.abehsera@gmail.com>
2019-01-26 15:55:52 -0500

    added project

---
+++ Insertion-Sort/c-sharp/insertion-sort-c-sharp/insertion-sort-c-sharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace insertionsortcsharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
