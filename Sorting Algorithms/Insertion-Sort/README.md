3c580e663fdb02f682560fabc7a6bed3f0146a28
Mikael Abehsera <mikael.abehsera@gmail.com>
2019-01-26 20:36:25 -0500

    added to inserstion

--- Sorting Algorithms/Insertion-Sort/README.md
+++ Sorting Algorithms/Insertion-Sort/README.md
@@ -3,7 +3,6 @@
 With Insertion Sort we compare the current element with the element ahead of it. Lets use a unsorted array of numbers as an example, if the number ahead is smaller then the current number then we can toss is behind and keep going with the current number. All the numbers behind the current number will also get sorted as we move down the array. Insertion sort is very similar to how most people organise a hand of playing cards.
 
 ## Complexity
-
 | Best            | Average             | Worst               |
 | :-------------: | :-----------------: | :-----------------: | 
 | n               | n<sup>2</sup>       | n<sup>2</sup>       |
@@ -16,7 +15,6 @@ With Insertion Sort we compare the current element with the element ahead of it.
 3. repeat step 2 recursivly until there are no more numbers in the array. 
 
 ## Examples
-
 ![Sorting example](https://thumbs.gfycat.com/SilkyBrownHoverfly-size_restricted.gif)
 ![Sorting example](https://thumbs.gfycat.com/CornyThickGordonsetter-small.gif)
 ![Sorting example](https://thumbs.gfycat.com/UnfinishedBoldGenet-size_restricted.gif)
---
+++ Sorting Algorithms/Insertion-Sort/c-sharp/insertion-sort-c-sharp/insertion-sort-c-sharp/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace insertionsortcsharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] array = new int[] { 10, 12, 3, 4, 5, 65, 454, 32, 14, 63, 34, 343, 2356, 123, 143, 321, 432, 156 };
+            //Console.WriteLine("program started");
+            //Console.WriteLine(insertionSort(array, 0));
+            //Console.WriteLine("program ended");
+            //Console.ReadLine();
+
+            Console.WriteLine(insert(array, 24));
+            Console.ReadLine();
+
+        }
+
+        public static List<int> insert(int[] array, int number)
+        {
+            List<int> newArray = new List<int>();
+            bool hasBeenInserted = false;
+
+            if (!hasBeenInserted)
+            {
+                if (array[0] > number)
+                {
+                    newArray.Add(number);
+                    hasBeenInserted = true;
+                }
+            }
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (!hasBeenInserted)
+                {
+                    if (array[0] > number)
+                    {
+                        newArray.Add(number);
+                        hasBeenInserted = true;
+                    }
+                }
+                else
+                {
+                    newArray.Add(array[i]);
+                }
+            }
+
+            return newArray;
+        }
+
+        //public static int[] insertionSort(int[] array, int pointer)
+        //{
+
+        //}
+    }
+}
