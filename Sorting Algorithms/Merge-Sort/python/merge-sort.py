851d8b9e9be919975321bb9a2cc603eb7b3c05dc
Mikael Abehsera <mikael.abehsera@gmail.com>
2019-01-29 18:25:56 -0500

    python mergesort template

--- Sorting Algorithms/Merge-Sort/python/merge-sort.py
+++ Sorting Algorithms/Merge-Sort/python/merge-sort.py
@@ -1,7 +1,17 @@
 def merge(left, right):
+  result = []
+  leftIndex = 0
+  rightIndex = 0
+  while leftIndex < len(left) and rightIndex < len(right):
+    if left[leftIndex] <= right[rightIndex]:
+      result.append(left[leftIndex])
+      leftIndex += 1
+    else:
+      result.append(right[rightIndex])
+      rightIndex += 1
 
 def mergesort(m):
- 
+
 
 array = [2000, 8, 1, 4, 14, 7, 16, 10, 9, 3, 53, 64, 2435, 234, 123, 135, 6, 76, 543]
 sortedArray = mergesort(array)
